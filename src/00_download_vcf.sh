bbeefc8b881353fdd913c6a9bfeb3118faa098be
Binglan Li <bl.victoria.li@gmail.com>
2022-04-01 11:31:21 -0700

    update: download X chromosome

--- src/00_download_vcf.sh
+++ src/00_download_vcf.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500M
 #SBATCH --output=dl_vcf.%A_%a.log
-#SBATCH --array=0-10
+#SBATCH --array=10
 
 ## Author:Binglan Li
 ## Date: 03/04/2022
@@ -22,7 +22,7 @@ cd "$RAW_VCF_DIR"
 #######################################################
 ## 01. Download VCFs
 #######################################################
-CHR_LIST=(chr1 chr2 chr4 chr6 chr7 chr10 chr12 chr13 chr16 chr19 others)
+CHR_LIST=(chr1 chr2 chr4 chr6 chr7 chr10 chr12 chr13 chr16 chr19 chrX others)
 FTP_SITE=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20201028_3202_raw_GT_with_annot/
 wget "$FTP_SITE"20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_"${CHR_LIST["$SLURM_ARRAY_TASK_ID"]}".recalibrated_variants.vcf.gz
 wget "$FTP_SITE"20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_"${CHR_LIST["$SLURM_ARRAY_TASK_ID"]}".recalibrated_variants.vcf.gz.tbi
