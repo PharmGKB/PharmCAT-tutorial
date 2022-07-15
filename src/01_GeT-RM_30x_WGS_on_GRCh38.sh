1da77f3e70246165806ae0a3a3141985f757418f
Binglan Li <bl.victoria.li@gmail.com>
2022-07-14 22:53:16 -0700

    update: add tutorial vcf input

--- src/01_GeT-RM_30x_WGS_on_GRCh38.sh
+++ src/01_GeT-RM_30x_WGS_on_GRCh38.sh
@@ -5,13 +5,13 @@
 
 # work under the current tutorial directory
 PROJECT_DIR="$PWD"
-RAW_VCF_DIR="$PROJECT_DIR"/raw_vcf/
+RAW_VCF_DIR="$PROJECT_DIR"/data/raw_vcf/
 cd "$PROJECT_DIR"
 
 # path to the tutorial GeT-RM sample list
 TEST_SAMPLES=data/test_get-rm_samples.txt
-# manually prepared PGx regions based on "PharmCAT/pharmcat_positions.vcf" as of v1.5.1
-PGX_GENE_REGION_FILE=pharmcat_positions_w_1Mbp_padding_v1.5.1.txt
+# manually prepared PGx regions based on "PharmCAT/pharmcat_positions.vcf"
+PGX_GENE_REGION_FILE=pharmcat_positions_w_1Mbp_padding_regions.txt
 
 # define the desired VCF file name that will be used for the tutorial
 TUTORIAL_VCF=PharmCAT_tutorial_get-rm_wgs_30x_grch38.vcf.gz
@@ -23,7 +23,7 @@ TO_BE_CONCAT_FILE_LIST="$RAW_VCF_DIR"to_be_concatenated_file_list.txt
 # remove if a $TO_BE_CONCAT_FILE_LIST already exists
 if [ -f "$TO_BE_CONCAT_FILE_LIST" ]; then rm "$TO_BE_CONCAT_FILE_LIST"; fi
 # list files
-CHR_LIST=(1 2 4 6 7 10 12 13 16 19)
+CHR_LIST=(1 2 4 6 7 10 12 13 16 19 22)
 for CHR in "${CHR_LIST[@]}"; do
    bcftools view \
    -S "$TEST_SAMPLES" \
